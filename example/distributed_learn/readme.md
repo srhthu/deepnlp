9eb560fc2ea14c61c2122afb649116cb9522af96
srhthu <shuirh2019@gmail.com>
2023-07-24 16:20:14 +0800

    Write readme and demo of distributed training

--- example/distributed_learn/readme.md
+++ example/distributed_learn/readme.md
@@ -1,5 +1,5 @@
 # Distributed training
-## Pipeline of distributed parallel
+## Startup of Distributed Training
 We introduce how to start a distributed training and the configuration process.
 ### Lunch
 The multiple processes are lunched with related variables, e.g., local rank, which are passed through two ways:  
@@ -34,3 +34,34 @@ import os
 model.to(int(os.environs["LOCAL_RANK"]))
 ```
 Use `LOCAL_RANK` is critical if there are more than one nodes.
+
+### Issues
+#### gloo backend connection refused
+If only initialize the nccl backend with two nodes, it works
+```Bash
+# node 1
+torchrun --nproc_per_node=2 --master_addr 172.28.176.19 --master_port 15432 --nnodes 2 --node_rank 0 start.py
+# node 2
+torchrun --nproc_per_node=2 --master_addr 172.28.176.19 --master_port 15432 --nnodes 2 --node_rank 1 start.py
+```
+However, if initialize with gloo backend, either by default or explicitly pass the argumetn. The error message of each process looks like:
+```
+RuntimeError connect [127.0.1.1]:<port>: Connection refused
+```
+the port is different for each process.
+
+But `gloo` works for single node with multiple workers.
+
+Note: `nccl` is optimized for CUDA tensors. Recommended for CUDA collective operations.
+
+## Communication
+We introduce the collective communication here, while there are also other communication types e.g. Point-to-Point but rarely accessed in training for general users.
+
+Some functions:
+- reduce
+- all_reduce
+- gather
+- all_gather
+
+See demos in `start.py`
+
--- example/distributed_learn/start.py
+++ example/distributed_learn/start.py
@@ -7,15 +7,81 @@ import torch.distributed as dist
 mp_vars = ['LOCAL_RANK', 'RANK', 'LOCAL_WORLD_SIZE', 'WORLD_SIZE', 'MASTER_ADDR', 'MASTER_PORT']
 print({k:v for k,v in os.environ.items() if k in mp_vars})
 print(sys.argv)
-dist.init_process_group()
-lr = dist.get_rank()
-if lr == 0:
-    print(lr)
-    print(torch.cuda.device_count())
 
-model = nn.Linear(2,3)
-model.to(int(os.environ['LOCAL_RANK']))
-print(model.weight.device)
+if os.environ['LOCAL_RANK'] == '0':
+    # print(os.environ)
+    ...
+
+dist.init_process_group('nccl')
+# gloo do not support multi nodes.
+
+rank = dist.get_rank()
+world_size = dist.get_world_size()
+local_rank = int(os.environ['LOCAL_RANK'])
+if rank == 0:
+    print('GPUs: ', torch.cuda.device_count())
+
+def demo_all_reduce():
+    if local_rank == 0:
+        print('Example of all_reduce')
+    tensor = torch.Tensor([rank]).cuda(local_rank)
+    print(f'Before all_reduce: Rank {rank} {tensor}')
+    dist.all_reduce(tensor)
+    print(f'After all_reduce: Rank {rank} {tensor}')
+    dist.barrier()
+    if local_rank == 0:
+        print('='*30)
+
+def demo_reduce():
+    if local_rank == 0:
+        print('Example of reduce')
+    tensor = torch.Tensor([rank]).cuda(local_rank)
+    print(f'Before reduce: Rank {rank} {tensor}')
+    dist.reduce(tensor, 0) # all workers should report the tensor to worker_0
+    print(f'After reduce: Rank {rank} {tensor}')
+    dist.barrier()
+    if local_rank == 0:
+        print('='*30)
+
+def demo_all_gather():
+    if local_rank == 0:
+        print('Example of all_gather')
+    tensor = torch.Tensor([rank]).cuda(local_rank)
+    print(f'Before all_gather: Rank {rank} {tensor}')
+    tensor_list = [torch.zeros(tensor.shape).cuda(local_rank) for _ in range(world_size)]
+    # It is critical to make tensor_list on local_rank GPU for nccl
+    dist.all_gather(tensor_list, tensor)
+    print(f'After all_gather: Rank {rank} {tensor_list}')
+    dist.barrier()
+    if local_rank == 0:
+        print('='*30)
+
+def demo_gather():
+    if local_rank == 0:
+        print('Example of gather')
+    tensor = torch.Tensor([rank]).cuda(local_rank)
+    print(f'Before gather: Rank {rank} {tensor}')
+    if rank == 0:
+        tensor_list = [torch.zeros(tensor.shape).cuda(local_rank) for _ in range(world_size)]
+    else:
+        tensor_list = None # critical to set to None for other workers
+    dist.gather(tensor, tensor_list, dst = 0)
+    print(f'After gather: Rank {rank} {tensor_list}')
+    dist.barrier()
+    if rank == 0:
+        ave = torch.mean(torch.cat(tensor_list))
+        print(f'Average: {ave}')
+    if local_rank == 0:
+        print('='*30)
+
+demo_all_reduce()
+demo_reduce()
+demo_all_gather()
+demo_gather()
 """
 torchrun --nproc_per_node=2  start.py
+
+torchrun --nproc_per_node=2 --master_addr 172.28.176.19 --master_port 15432  start.py
+
+torchrun --nproc_per_node=2 --master_addr 172.28.176.19 --master_port 15432 --nnodes 2 --node_rank 0 start.py
 """
